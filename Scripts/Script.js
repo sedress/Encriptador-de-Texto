f64a8d6f7f389c35d37b863adf5ed988397ed0d3
Sedress <sedress12@gmail.com>
2024-08-04 14:21:40 -0500

    se actualizo el js

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,13 +1,48 @@
-//función para encriptar texto
+//Restricciones
+function restricciones(texto) {
+    return /^[a-z\s]+$/.test(texto);
+}
+
+//Encriptar texto
 function encriptar(texto) {
     let encriptacion = {'a': "ai", 'e': "enter", 'i':"imes",'o':"ober",'u':"ufat"};
     return texto.replace(/[aeiou]/g, match => encriptacion[match]);
 };
 
-//función para desencriptar texto
+//Desencriptar texto
 function desencriptar(texto) {
     let desencriptacion = {"ai": 'a', "enter": 'e', "imes": 'i', "ober": 'o', "ufat": 'u'};
     return texto.replace(/ai|enter|imes|ober|ufat/g, match => desencriptacion[match]);
+}
+
+//Mostrar el mensaje de advertencia
+function mostrarMensajeAdvertencia() {
+    let advertencia = document.querySelector(".advertencia-1");
+    advertencia.style.display = "block";
+}
+
+//Ocultar el mensaje de advertencia
+function ocultarMensajeAdvertencia() {
+    let advertencia = document.querySelector(".advertencia-1");
+    advertencia.style.display = "none";
+}
+
+// Función para mostrar elementos de la segunda sección
+function mostrarSegundaSeccion() {
+    document.querySelector(".input-text-2").style.display = "block";
+    document.querySelector("#boton-copiar").style.display = "block";
+    document.querySelector(".Muñeco").style.display = "none";
+    document.querySelector(".segunda-seccion-texto-1").style.display = "none";
+    document.querySelector(".segunda-seccion-texto-2").style.display = "none";
+}
+
+// Función para ocultar elementos de la segunda sección
+function ocultarSegundaSeccion() {
+    document.querySelector(".input-text-2").style.display = "none";
+    document.querySelector("#boton-copiar").style.display = "none";
+    document.querySelector(".Muñeco").style.display = "block";
+    document.querySelector(".segunda-seccion-texto-1").style.display = "block";
+    document.querySelector(".segunda-seccion-texto-2").style.display = "block";
 }
 
 //Botones
@@ -15,15 +50,29 @@ function desencriptar(texto) {
 //Botón Encriptar
 document.getElementById("boton-encriptar").addEventListener("click", () => {
     let inputTexEncriptar = document.getElementById("input-text-1").value;
-    let ResultadoEncriptar = encriptar(inputTexEncriptar);
-    document.getElementById("input-text-2").innerText = ResultadoEncriptar;
+        if (restricciones(inputTexEncriptar)) {
+            ocultarMensajeAdvertencia();
+            let ResultadoEncriptar = encriptar(inputTexEncriptar);
+            document.getElementById("input-text-2").innerText = ResultadoEncriptar;
+            mostrarSegundaSeccion();
+        } else {
+            mostrarMensajeAdvertencia();
+            ocultarSegundaSeccion();
+    }
 });
 
 //Botón Desencriptar
 document.getElementById("boton-desencriptar").addEventListener("click", () => {
     let inputTexDesencriptar = document.getElementById("input-text-1").value;
-    let ResultadoDesencriptar = desencriptar(inputTexDesencriptar)
-    document.getElementById("input-text-2").innerText = ResultadoDesencriptar;
+        if (restricciones(inputTexDesencriptar)) {
+            ocultarMensajeAdvertencia();
+            let ResultadoDesencriptar = desencriptar(inputTexDesencriptar);
+            document.getElementById("input-text-2").innerText = ResultadoDesencriptar;
+            mostrarSegundaSeccion();
+    } else {
+            mostrarMensajeAdvertencia();
+            ocultarSegundaSeccion();
+    }
 });
 
 //Botón Copiar
